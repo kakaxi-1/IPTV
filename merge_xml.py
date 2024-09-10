1756889035abb2deaf255b231da33d6cff2e1799
kakaxi <163295755+kakaxi-1@users.noreply.github.com>
2024-09-11 04:39:59 +0800

    Update merge_xml.py

--- merge_xml.py
+++ merge_xml.py
@@ -4,7 +4,7 @@ def merge_xml_files(files):
     try:
         root = None
         for file in files:
-            # 读取文件时指定编码
+
             with open(file, 'r', encoding='utf-8') as f:
                 tree = ET.parse(f)
                 if root is None:
@@ -12,7 +12,7 @@ def merge_xml_files(files):
                 else:
                     root.extend(tree.getroot())
 
-        # 输出合并后的 XML 文件，指定编码
+
         tree = ET.ElementTree(root)
         with open('epg.xml', 'wb') as f:
             tree.write(f, encoding='utf-8', xml_declaration=True)
@@ -22,5 +22,5 @@ def merge_xml_files(files):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-# 使用文件名列表调用函数
+
 merge_xml_files(["e.xml", "epg_part.xml"])
