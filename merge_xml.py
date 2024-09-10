caf8012be8b55ee1afb870969e6288034795ddf9
kakaxi <163295755+kakaxi-1@users.noreply.github.com>
2024-09-11 04:22:20 +0800

    Update merge_xml.py

--- merge_xml.py
+++ merge_xml.py
@@ -1,16 +1,18 @@
 import xml.etree.ElementTree as ET
 
-# 加载第一个XML文件
-tree1 = ET.parse('e.xml')
-root1 = tree1.getroot()
+def merge_xml_files(files):
+    root_elements = []
+    for file in files:
+        tree = ET.parse(file)
+        root_elements.append(tree.getroot())
+    
+    merged_root = ET.Element("root")  # 创建一个根元素
+    for root in root_elements:
+        for child in root:
+            merged_root.append(child)
+    
+    merged_tree = ET.ElementTree(merged_root)
+    merged_tree.write("epg.xml", encoding="utf-8", xml_declaration=True)
 
-# 加载第二个XML文件
-tree2 = ET.parse('epg_part.xml')
-root2 = tree2.getroot()
-
-# 将第二个XML文件的内容添加到第一个XML的根节点下
-for elem in root2:
-    root1.append(elem)
-
-# 保存合并后的XML文件
-tree1.write('epg.xml')
+if __name__ == "__main__":
+    merge_xml_files(["e.xml", "epg_part.xml"])
