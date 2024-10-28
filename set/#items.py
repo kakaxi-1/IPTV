3981bbfab27a8706544929cd199cca60705ae872
kakaxi <163295755+kakaxi-1@users.noreply.github.com>
2024-10-29 01:11:50 +0800

    更新 #items.py

--- set/#items.py
+++ set/#items.py
@@ -5,11 +5,9 @@ import base64
 import cv2
 from datetime import datetime
 
-FOFA_EMAIL = 'ittnxpfh9@gmail.com'
-FOFA_KEY = '493bca542ade43fed47fff733a4242af'
 
-os.makedirs('rtp', exist_ok=True)
-os.makedirs('txt_files', exist_ok=True)
+FOFA_EMAIL = 'ittnxpfh9@gmail.com'  
+FOFA_KEY = '493bca542ade43fed47fff733a4242af'
 
 files = os.listdir('rtp')
 files_name = [os.path.splitext(file)[0] for file in files]
@@ -30,21 +28,6 @@ for province_isp in provinces_isps:
     except FileNotFoundError:
         print(f"文件 '{province_isp}.txt' 不存在. 跳过此文件.")
 
-def get_fofa_results(province, org):
-
-    query = f'"udpxy" && country="CN" && region="{province}" && org="{org}"'
-    query_base64 = base64.b64encode(query.encode('utf-8')).decode('utf-8')
-    api_url = f"https://fofa.info/api/v1/search/all?email={FOFA_EMAIL}&key={FOFA_KEY}&qbase64={query_base64}&size=100"
-
-    try:
-        response = requests.get(api_url, timeout=10)
-        response.raise_for_status()
-        results = response.json().get("results", [])
-        return [result[0] for result in results]
-    except requests.RequestException as e:
-        print(f"Fofa API 请求失败: {e}")
-        return []
-
 for keyword in keywords:
     province, isp, mcast = keyword.split("_")
     
@@ -64,36 +47,60 @@ for keyword in keywords:
         continue
 
     current_time = datetime.now()
-    result_urls = get_fofa_results(province, org)
-    print(f"{current_time} 查询运营商 : {province}{isp} ，结果 URLs : {result_urls}")
+    timeout_cnt = 0
+    result_urls = set() 
+    while len(result_urls) == 0 and timeout_cnt <= 5:
+        try:
+   
+            search_txt = f'"udpxy" && country="CN" && region="{province}" && org="{org}"'
+            query_base64 = base64.b64encode(search_txt.encode('utf-8')).decode('utf-8')
+            api_url = f"https://fofa.info/api/v1/search/all?email={FOFA_EMAIL}&key={FOFA_KEY}&qbase64={query_base64}&size=100"
 
-    valid_ips = []
-    for url in result_urls:
-        video_url = f"{url}/rtp/{mcast}"
-        cap = cv2.VideoCapture(video_url)
-        
-        if cap.isOpened():
-            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
+            print(f"{current_time} 查询运营商 : {province}{isp} ，查询网址 : {api_url}")
+            response = requests.get(api_url, timeout=5)
+            response.raise_for_status()
 
-            if width > 0 and height > 0:
-                valid_ips.append(url)
-            cap.release()
-        else:
-            print(f"{current_time} {video_url} 无效")
     
-    if valid_ips:
-        rtp_filename = f'rtp/{province}_{isp}.txt'
-        with open(rtp_filename, 'r', encoding='utf-8') as file:
-            data = file.read()
-        txt_filename = f'txt_files/{province}{isp}.txt'
-        
-        with open(txt_filename, 'w', encoding='utf-8') as new_file:
-            for url in valid_ips:
-                new_data = data.replace("rtp://", f"{url}/rtp/")
-                new_file.write(new_data)
+            results = response.json().get("results", [])
+            result_urls = set(result[0] for result in results)
+            print(f"{current_time} result_urls:{result_urls}")
 
-        print(f'已生成播放列表，保存至 {txt_filename}')
+            valid_ips = []
+
+            for url in result_urls:
+                video_url = f"{url}/rtp/{mcast}"
+                cap = cv2.VideoCapture(video_url)
+                
+                if cap.isOpened():
+                    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
+
+                    if width > 0 and height > 0:
+                        valid_ips.append(url)
+                    cap.release()
+                else:
+                    print(f"{current_time} {video_url} 无效")
+            
+            if valid_ips:
+                rtp_filename = f'rtp/{province}_{isp}.txt'
+                with open(rtp_filename, 'r', encoding='utf-8') as file:
+                    data = file.read()
+                txt_filename = f'txt_files/{province}{isp}.txt'
+                
+                with open(txt_filename, 'w', encoding='utf-8') as new_file:
+                    for url in valid_ips:
+                        new_data = data.replace("rtp://", f"{url}/rtp/")
+                        new_file.write(new_data)
+
+                print(f'已生成播放列表，保存至 {txt_filename}')
+
+        except (requests.Timeout, requests.RequestException) as e:
+            timeout_cnt += 1
+            print(f"{current_time} [{province}] 搜索请求发生超时，异常次数：{timeout_cnt}")
+            if timeout_cnt <= 3:
+                continue
+            else:
+                print(f"{current_time} 搜索IPTV频道源[{province}{isp}]，超时次数过多：{timeout_cnt} 次，停止处理")
 
 print('节目表制作完成！ 文件输出在 txt_files 目录下！')
