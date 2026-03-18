99d98c4ce7e33e42aa2431536e3fe59b5da66091
GitHub Action <action@github.com>
2026-03-18 20:24:56 +0000

    Update JS files (automated)

--- omnibox_js/Film-艾旦影视.js
+++ omnibox_js/Film-艾旦影视.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 const https = require("https");
 const OmniBox = require("omnibox_sdk");
 
-const API_HOST = 'https://pz.168188.dpdns.org';
-const API_URL = 'https://pz.168188.dpdns.org/?url=https://lovedan.net/api.php/provide/vod';
+const API_HOST = 'https://lovedan.net';
+const API_URL = 'https://lovedan.net/api.php/provide/vod';
 
 const def_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
